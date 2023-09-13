c8a092a212f6989ae01043e58772e8b6b042d75a
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-09-14 02:00:40 +0530

    pattern questions

---
+++ patterns/halfPyramid.js
@@ -0,0 +1,22 @@
+let n = 5;
+
+function halfPyramid(n) {
+  for (let i = 0; i < n; i++) {
+    let str = '';
+    for (let j = 0; j <= i; j++) {
+      str += '*';
+    }
+    console.log(str);
+  }
+}
+
+halfPyramid(n);
+
+/*
+n  = 5 so 5 rows total and each row will have that number of starts
+*
+**
+***
+****
+*****
+*/
