414b52d560b4fbe15c6d4b92fb1504366b26354a
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-09-15 02:10:28 +0530

    check pallindrome number

--- Maths/countDigits.js
+++ Maths/countDigits.js
@@ -1,14 +1,31 @@
-// count total number of digits in a number
+/* count total number of digits in a number
+ approach 1: iteatively
+ approach 2: recursively
+ approach 3: log10
+*/
 
 let n = 83838838388;
 
 const countDigits = (n) => {
   let count = 0;
   while (n > 0) {
-    count++;
+    ++count;
     n = Math.floor(n / 10);
   }
   return count;
 };
-const answer = countDigits(n);
-console.log(answer);
+const answer1 = countDigits(n);
+console.log(answer1);
+
+//recursive
+
+const countDigitsRecursive = (n) => {
+  if (n === 0) return 0;
+  return 1 + countDigitsRecursive(Math.floor(n / 10));
+};
+const answer2 = countDigitsRecursive(n);
+console.log(answer2);
+
+//using Log10
+console.log(Math.floor(Math.log10(n) + 1));
+console.log(Math.floor(Math.log10(100000)));
---
+++ Maths/numberPallindrome.js
@@ -0,0 +1,34 @@
+/* check whether a number is pallindrome of not
+Example1
+  373  0*10+3 = 3
+  37   3*10+7 = 37
+  3    37*10+3 = 373
+  0    373
+
+  373==373 true
+
+Example 2 
+  487  0*10+7 = 7
+  48   7*10+8 = 78
+  4    78*10+4 = 784
+  0    784
+
+  487 !== 784 false
+    
+*/
+
+let n = 99877899;
+
+const pallindromeCheck = (n) => {
+  let rem = 0;
+  let copy = n;
+
+  while (n > 0) {
+    rem = rem * 10 + Math.floor(n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  return rem === copy;
+};
+const answer = pallindromeCheck(n);
+console.log(answer);
