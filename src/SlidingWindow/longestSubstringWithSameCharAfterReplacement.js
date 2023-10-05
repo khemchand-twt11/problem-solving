20e494453247284bcdecccbf4dd18b8dc7deb710
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-10-05 23:56:00 +0530

    longest substring with same character after replacement

--- src/SlidingWindow/longestSubstringWithSameCharAfterReplacement.js
+++ src/SlidingWindow/longestSubstringWithSameCharAfterReplacement.js
@@ -1,7 +1,5 @@
 /*
-Problem: You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
-
-Return the length of the longest substring containing the same letter you can get after performing the above operations.
+Problem: Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
 
 
 Examples:
@@ -22,19 +20,21 @@ Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating subst
 function longestSubstringWithSameCharAfterReplacement(str, k) {
   let windowStart = 0;
   let maxLen = 0;
+  let maxRepeatingCharCount = 0;
   let map = new Map();
 
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     const char = str[windowEnd];
     map.set(char, (map.get(char) || 0) + 1);
-    while (map.size - k > 1) {
+    maxRepeatingCharCount = Math.max(maxRepeatingCharCount, map.get(char));
+    if (windowEnd - windowStart + 1 - maxRepeatingCharCount > k) {
       const charToRemove = str[windowStart];
       map.set(charToRemove, map.get(charToRemove) - 1);
       if (map.get(charToRemove) === 0) map.delete(charToRemove);
       windowStart++;
     }
 
-    maxLen = Math.max(windowEnd - windowStart + 1);
+    maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
   }
   return maxLen;
 }
